6fa3e31839bc1ed1e4925d7bb687e1366bef9c55
o-b3d <obed@ogtesting.com>
2023-10-29 20:01:00 -0400

    hopefully final commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //
+  //Event Listener to change banner opacity based on value of select tag
   const selectBanner = document.getElementById("selectBanner");
   selectBanner.addEventListener("change", viewBanner);
 
@@ -16,22 +16,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  //
+  //Event Listener to change "Full Name" text in bio container based on value of text input
   const fullNameInput = document.getElementById("fullNameTextInput");
-  fullNameInput.addEventListener("input", updateHeader);
 
-  function updateHeader() {
-    const fullNameInputValue =
-      document.getElementById("fullNameTextInput").value;
+  fullNameInput.addEventListener("input", () => {
+    const fullNameInputValue = document.getElementById("fullNameTextInput").value;
     const fullNameContainer = document.querySelector("h1");
     fullNameContainer.innerText = fullNameInputValue;
-  }
+  });
 
-  //
+  //Event Listener that changes margin-left of social media icons based on value of select tag
   const positionSelected = document.getElementById("selectPosition");
-  positionSelected.addEventListener("change", updateSocialMediaIconsPosition);
 
-  function updateSocialMediaIconsPosition() {
+  positionSelected.addEventListener("change", () => {
     const positionValue = selectPosition.value;
     const socialMediaIconsContainer = document.querySelector(".socialMediaIconsContainer");
 
@@ -40,38 +37,65 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       socialMediaIconsContainer.style.marginLeft = "505px";
     }
-  }
+  });
 
-  //
+  //Event Listener that changes "Employment" text in bio container based on value of select tag
   const employmentSelection = document.getElementById("selectEmployment");
-  employmentSelection.addEventListener("change", updateEmployment);
 
-  function updateEmployment() {
+  employmentSelection.addEventListener("change", () => {
     const employmentSelectionValue = selectEmployment.value;
     const employmentContainer = document.querySelector("h2");
     employmentContainer.innerHTML = employmentSelectionValue;
-  }
+  });
 
-  //
+  //Event Listener that changes "City" text in bio container based on value of select tag
   const citySelection = document.getElementById("selectCity");
-  citySelection.addEventListener("change", updateCity);
 
-  function updateCity() {
+  citySelection.addEventListener("change", () => {
     const cityValue = selectCity.value;
     const cityContainer = document.querySelector("h3");
     cityContainer.innerHTML = cityValue;
-  }
+  });
 
-  //
+  //Event Listener that changes "County" text in bio container based on value of select tag
   const countySelection = document.getElementById("selectCounty");
-  countySelection.addEventListener("change", updateCounty);
 
-  function updateCounty() {
+  countySelection.addEventListener("change", () => {
     const countyValue = selectCounty.value;
     const countyContainer = document.querySelector("h4");
     countyContainer.textContent = countyValue;
-  }
+  });
 
-  //
-  
+  //Event Listener that changes URL of GitHub icon based on input value
+const githubInput = document.getElementById("githubInput");
+const githubURL = document.getElementById("githubURL");
+
+githubInput.addEventListener("input", () => {
+  githubURL.href = `https://github.com/${githubInput.value}`;
 });
+
+//
+const xInput = document.getElementById("xInput");
+const xURL = document.getElementById("xURL");
+
+xInput.addEventListener("input", () => {
+xURL.href = `https://x.com/${xInput.value}`;
+});
+
+//
+const linkedinInput = document.getElementById("linkedinInput");
+const linkedinURL = document.getElementById("linkedinURL");
+
+linkedinInput.addEventListener("input", () => {
+linkedinURL.href = `https://www.linkedin.com/in/${linkedinInput.value}`;
+});
+
+//
+const instagramInput = document.getElementById("instagramInput");
+const instagramURL = document.getElementById("instagramURL");
+
+instagramInput.addEventListener("input", () => {
+instagramURL.href = `https://www.instagram.com/${instagramInput.value}`;
+});
+
+})
